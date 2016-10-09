ca9399a2ec49eb2c5a338ba0d078b84e0ce41d1b
Carlos Galdamez <cgaldamez14@gmail.com>
2016-10-09 10:30:04 -0700

    implemented address class, wallet needs to be mapped in wallet class

--- src/main/java/bitfire/model/Address.java
+++ src/main/java/bitfire/model/Address.java
@@ -4,11 +4,63 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 @Table(name="addresses")
 public class Address implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue
+	@Column(name="address_id")
+	String addressId;
+	
+	String address;
+	
+	String label;
+	
+	@ManyToOne  // Needs to be mapped in Wallet Class. Many to one does not have 'mappedBy' feature
+	Wallet wallet;
+
+	public String getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Wallet getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+	
+	
 
 }
